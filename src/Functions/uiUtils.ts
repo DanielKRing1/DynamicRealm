7f7ea768564afb9bf58532f656ab4d76b60b1a7c
DanielKRing1 <DanielKRing1@gmail.com>
2021-11-03 01:47:01 -0700

    Replace Array.flat method, which is not supported in react native

--- src/Functions/uiUtils.ts
+++ src/Functions/uiUtils.ts
@@ -21,7 +21,8 @@ export function getSchemaNames(realmPath: string = undefined): string[] {
         // 1.2.1. Get all DynamicRealms
         const dynamicRealmSchemas: DynamicRealmProperties[] = Array.from(globalRealm.getRealm().objects(DYNAMIC_REALM_NAME));
         // 1.2.2. Flatten all schema names into one list
-        schemaNames = dynamicRealmSchemas.map((dynamicRealmSchema: DynamicRealmProperties) => dynamicRealmSchema.schemaNames).flat();
+        schemaNames = [];
+        dynamicRealmSchemas.forEach((dynamicRealmSchema: DynamicRealmProperties) => schemaNames.push(...dynamicRealmSchema.schemaNames));
     }
 
     // 2. Get DynamicSchemas
