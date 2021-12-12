295ac86c3160459f617dd1698626fc064c336752
DanielKRing1 <DanielKRing1@gmail.com>
2021-12-12 01:37:05 -0800

    Export isInitialized method + new npm build

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { saveSchema, saveSchemas, getSchema, getSchemas, rmSchema, rmSchemas } from './Functions/dynamicSchemaOperations';
 import { getMetadata, updateMetadata } from './Functions/metadataOperations';
-import { init, loadRealm, loadRealmFromSchemas } from './Functions/realmOperations';
+import { init, isInitialized, loadRealm, loadRealmFromSchemas } from './Functions/realmOperations';
 import { getRealmNames, getSchemaNames, getProperties } from './Functions/uiUtils';
 import { ARRAY_METADATA_HANDLERS, DICT_METADATA_HANDLERS } from './Metadata/defaultHandlers';
 
 export default {
     init,
+    isInitialized,
     saveSchema,
     saveSchemas,
 
